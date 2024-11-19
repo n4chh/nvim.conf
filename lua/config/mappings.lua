fea16c8676a6dc346a070de145d6747e0b8c28e9
iortego42 <iortego42madrid@gmail.com>
2024-11-19 21:44:20 +0100

    Nuevos mapings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -6,8 +6,10 @@ wk.add({
       return require("which-key.extras").expand.buf()
     end,
   },
-  { "<leader>b[", "<cmd>bp<cr>", desc = "Buffer anterior"},
-  { "<leader>b]", "<cmd>bn<cr>", desc = "Buffer posterior"},
+  -- Después de nvim 10, K se habilitó por defecto al hover del
+-- lsp.
+  { "[b", "<cmd>bp<cr>", desc = "Buffer anterior"},
+  { "]b", "<cmd>bn<cr>", desc = "Buffer posterior"},
   { "<leader>W", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
   {
     mode = { "n", "v" }, -- NORMAL and VISUAL mode
