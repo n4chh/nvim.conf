5e034f05f06160521f56a2005343554980406a64
n4chh <iortego42madrid@gmail.com>
2025-04-08 21:25:50 +0200

    Solucionado que se cierre la interfaz de debug cuando finaliza el programa

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -129,12 +129,12 @@ dap.listeners.before.launch.dapui_config = function()
 	dapui.open()
 end
 
-dap.listeners.before.event_terminated.dapui_config = function()
-	dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-	dapui.close()
-end
+-- dap.listeners.before.event_terminated.dapui_config = function()
+-- 	dapui.close()
+-- end
+-- dap.listeners.before.event_exited.dapui_config = function()
+-- 	dapui.close()
+-- end
 dapui.setup()
 
 
