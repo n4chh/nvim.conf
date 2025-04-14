98efc9ba763f53c59a31b628c8b9e3c9a6f84aa6
n4chh <iortego42madrid@gmail.com>
2025-04-14 13:41:35 +0200

    cambio al colorscheme kanagawa por compatibilidad con plugins

--- after/plugin/autodarkmode.lua
+++ after/plugin/autodarkmode.lua
@@ -4,11 +4,11 @@ local adm = require('auto-dark-mode')
 adm.setup({
     set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-		vim.cmd('colorscheme github_dark')
+		vim.cmd('colorscheme kanagawa')
     end,
     set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
-		vim.cmd('colorscheme github_light')
+		vim.cmd('colorscheme kanagawa-lotus')
     end,
     update_interval = 3000,
     fallback = "dark"
