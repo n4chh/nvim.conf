f4f71843e4acc1ae93dfa1fdb1c3681f063c03d9
n4chh <iortego42madrid@gmail.com>
2025-04-22 15:44:30 +0200

    Primer commit, instalacion lazy

---
+++ lua/config/plugins.lua
@@ -0,0 +1,64 @@
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	-- Temas
+	use 'Mofiqul/vscode.nvim'
+	use 'loctvl842/monokai-pro.nvim'
+	use 'projekt0n/github-nvim-theme'
+	use "rebelot/kanagawa.nvim"
+	use "echasnovski/mini.icons"
+	use "kepano/flexoki-neovim"
+
+	-- UI
+	use {
+	'folke/noice.nvim',
+	requires = {
+		    "MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify"
+	}
+	}
+	-- Telescope
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
+	-- Barra
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
+	-- Sintaxis y cosas varias
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use({ 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' })
+	use({ 'neovim/nvim-lspconfig' })
+	use({ 'hrsh7th/nvim-cmp' })
+	use({ 'hrsh7th/cmp-nvim-lsp' })
+	-- Explorador
+	use('nvim-tree/nvim-tree.lua')
+	use('nvim-tree/nvim-web-devicons')
+	-- Git
+	use('lewis6991/gitsigns.nvim')
+	use { 'NeogitOrg/neogit',
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		} }
+	-- LSP
+	use({ 'williamboman/mason.nvim' })
+	use({ 'williamboman/mason-lspconfig.nvim' })
+	-- Java
+	use 'mfussenegger/nvim-jdtls'
+	-- Debug
+	use({ 'mfussenegger/nvim-dap' })
+	use({ 'jay-babu/mason-nvim-dap.nvim' })
+	use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } }
+	-- TODO:
+	use({ 'folke/which-key.nvim' })
+	use 'f-person/auto-dark-mode.nvim'
+	use 'audibleblink/hackthebox.vim'
+	use 'navarasu/onedark.nvim'
+	use 'norcalli/nvim-colorizer.lua'
+
+	-- vim.cmd[[ PackerSync]]
+end)
