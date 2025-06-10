96647778080750ac26a2e84e9af753ee717db403
n4chh <iortego42madrid@gmail.com>
2025-06-10 09:09:28 +0200

    Switched to Tokyonight because of better performance

--- lua/plugins/autodarkmode.lua
+++ lua/plugins/autodarkmode.lua
@@ -2,11 +2,11 @@ return {
 	"f-person/auto-dark-mode.nvim",
 	config = function()
 		local adm = require("auto-dark-mode")
-
 		adm.setup({
 			set_dark_mode = function()
 				vim.api.nvim_set_option_value("background", "dark", {})
-				vim.cmd("colorscheme monokai-pro-machine")
+				-- vim.cmd("colorscheme monokai-pro-machine")
+				-- vim.cmd("colorscheme tokyonight")
 				-- vim.cmd("colorscheme github_dark")
 				-- onedark.setup {
 				-- 	style = 'darker',
@@ -39,7 +39,8 @@ return {
 			end,
 			set_light_mode = function()
 				vim.api.nvim_set_option_value("background", "light", {})
-				vim.cmd("colorscheme monokai-pro-light")
+				-- vim.cmd("colorscheme monokai-pro-light")
+				-- vim.cmd("colorscheme tokyonight-day")
 				-- vim.cmd("colorscheme github_light")
 				-- onedark.setup {
 				-- 	style = 'light',
